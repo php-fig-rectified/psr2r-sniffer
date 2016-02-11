a6ed902f9b4c317da7955f8348f8571279901b96
Mark Scherer <euromark@web.de>
2016-02-11 01:17:17 +0100

    Make script more dynamic.

--- bin/tokenize.php
+++ bin/tokenize.php
@@ -1,20 +1,22 @@
 #!/usr/bin/php -q
 <?php
 
-$minVersion = '5.4.16';
-if (file_exists('composer.json')) {
-    $composer = json_decode(file_get_contents('composer.json'));
-    if (isset($composer->require->php)) {
-        $minVersion = preg_replace('/([^0-9\.])/', '', $composer->require->php);
-    }
+/**
+ * Bin stub.
+ */
+$options = [
+	__DIR__ . '/../../../autoload.php',
+	__DIR__ . '/../../autoload.php',
+	__DIR__ . '/../vendor/autoload.php',
+	__DIR__ . '/vendor/autoload.php'
+];
+foreach ($options as $file) {
+	if (file_exists($file)) {
+		define('CODESNIFFER_COMPOSER_INSTALL', $file);
+		break;
+	}
 }
-if (version_compare(phpversion(), $minVersion, '<')) {
-    fwrite(STDERR, sprintf("Minimum PHP version: %s. You are using: %s.\n", $minVersion, phpversion()));
-    exit(-1);
-}
-
-
-require_once dirname(__DIR__) . '/vendor/autoload.php';
+require CODESNIFFER_COMPOSER_INSTALL;
 
 $tokenizer = new \PSR2R\Tools\Tokenizer($argv);
 
