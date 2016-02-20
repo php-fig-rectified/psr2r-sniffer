6ccaba7ddb9525d5363bfcdf7878f43cad9ebf2c
Mark Scherer <euromark@web.de>
2016-02-20 15:29:39 +0100

    Add new DocBlockParamAllowDefaultValueSniff

--- PSR2R/Sniffs/Commenting/DocBlockParamAllowDefaultValueSniff.php
+++ PSR2R/Sniffs/Commenting/DocBlockParamAllowDefaultValueSniff.php
@@ -37,8 +37,15 @@ class DocBlockParamAllowDefaultValueSniff extends AbstractSniff {
 			return;
 		}
 
+		$methodSignature = $this->getMethodSignature($phpCsFile, $stackPointer);
+		if (!$methodSignature) {
+			return;
+		}
+		//TODO: check count of signature vs doc block param count
+
 		$docBlockStartIndex = $tokens[$docBlockEndIndex]['comment_opener'];
 
+		$paramCount = 0;
 		for ($i = $docBlockStartIndex + 1; $i < $docBlockEndIndex; $i++) {
 			if ($tokens[$i]['type'] !== 'T_DOC_COMMENT_TAG') {
 				continue;
@@ -47,9 +54,17 @@ class DocBlockParamAllowDefaultValueSniff extends AbstractSniff {
 				continue;
 			}
 
+			if (empty($methodSignature[$paramCount])) {
+				$phpCsFile->addError('Param type does not have a matching signature in method', $i);
+				continue;
+			}
+			$methodSignatureValue = $methodSignature[$paramCount];
+			$paramCount++;
+
 			$classNameIndex = $i + 2;
 
 			if ($tokens[$classNameIndex]['type'] !== 'T_DOC_COMMENT_STRING') {
+				$phpCsFile->addError('Missing type in param doc block', $i);
 				continue;
 			}
 
@@ -61,17 +76,85 @@ class DocBlockParamAllowDefaultValueSniff extends AbstractSniff {
 				$appendix = substr($content, $spaceIndex);
 				$content = substr($content, 0, $spaceIndex);
 			}
-			if (empty($content) || strpos($content, '|') !== false) {
+			if (empty($content)) {
 				continue;
 			}
 
-			if (!in_array($content, ['null', 'false', 'true'], true)) {
+			if (empty($methodSignatureValue['typehint']) && empty($methodSignatureValue['default'])) {
 				continue;
 			}
 
-			$error = 'Possible doc block error: `' . $content . '` as only param type does not seem right. Makes this a no-op.';
-			$phpCsFile->addWarning($error, $i);
+			$pieces = explode('|', $content);
+			if ($methodSignatureValue['typehint']) {
+				$typeIndex = $methodSignatureValue['typehint'];
+				$type = $tokens[$typeIndex]['content'];
+				if (!in_array($type, $pieces)) {
+					$error = 'Possible doc block error: `' . $content . '` seems to be missing type `' . $type . '`.';
+					$fix = $phpCsFile->addFixableError($error, $classNameIndex);
+					if ($fix) {
+						$pieces[] = $type;
+						$content = implode('|', $pieces);
+						$phpCsFile->fixer->replaceToken($classNameIndex, $content . $appendix);
+					}
+				}
+			}
+			if ($methodSignatureValue['default']) {
+				$typeIndex = $methodSignatureValue['default'];
+				$type = $tokens[$typeIndex]['content'];
+				if (!in_array($type, $pieces)) {
+					$error = 'Possible doc block error: `' . $content . '` seems to be missing type `' . $type . '`.';
+					$fix = $phpCsFile->addFixableError($error, $classNameIndex);
+					if ($fix) {
+						$pieces[] = $type;
+						$content = implode('|', $pieces);
+						$phpCsFile->fixer->replaceToken($classNameIndex, $content . $appendix);
+					}
+				}
+			}
 		}
+	}
+
+	/**
+	 * @param \PHP_CodeSniffer_File $phpCsFile
+	 * @param int $stackPtr
+	 * @return array
+	 */
+	private function getMethodSignature(\PHP_CodeSniffer_File $phpCsFile, $stackPtr) {
+		$tokens = $phpCsFile->getTokens();
+
+		$startIndex = $phpCsFile->findNext(T_OPEN_PARENTHESIS, $stackPtr + 1);
+		$endIndex = $tokens[$startIndex]['parenthesis_closer'];
+
+		$arguments = [];
+		$i = $startIndex;
+		while ($nextVariableIndex = $phpCsFile->findNext(T_VARIABLE, $i + 1, $endIndex)) {
+			$typehint = $default = null;
+			$possibleTypeHint = $phpCsFile->findPrevious([T_ARRAY_HINT, T_CALLABLE], $nextVariableIndex - 1, $nextVariableIndex - 3);
+			if ($possibleTypeHint) {
+				$typehint = $possibleTypeHint;
+			}
+			if ($possibleTypeHint) {
+				$typehint = $possibleTypeHint;
+			}
+
+			$possibleEqualIndex = $phpCsFile->findNext([T_EQUAL], $nextVariableIndex + 1, $nextVariableIndex + 2);
+			if ($possibleEqualIndex) {
+				$possibleDefaultValue = $phpCsFile->findNext([T_STRING, T_TRUE, T_FALSE, T_NULL, T_ARRAY], $possibleEqualIndex + 1, $possibleEqualIndex + 2);
+				if ($possibleDefaultValue) {
+					$default = $possibleDefaultValue;
+				}
+			}
+
+			$arguments[] = [
+				'variable' => $nextVariableIndex,
+				'typehint' => $typehint,
+				'default' => $default
+			];
+
+			$i = $nextVariableIndex;
+		}
+
+		return $arguments;
 	}
 
 }
--- PSR2R/Sniffs/Commenting/DocBlockParamNoOpSniff.php
+++ PSR2R/Sniffs/Commenting/DocBlockParamNoOpSniff.php
@@ -6,6 +6,9 @@ use PSR2R\Tools\AbstractSniff;
 
 /**
  * Warn about `@param null $var` etc as a null/true/false would be a NO-OP.
+ *
+ * @author    Mark Scherer
+ * @license   MIT
  */
 class DocBlockParamNoOpSniff extends AbstractSniff {
 
