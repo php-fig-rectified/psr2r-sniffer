cf8c5b63927960df7b49886aca2cbb1d4256d2be
Mark Scherer <euromark@web.de>
2016-03-10 01:12:58 +0100

    Improve ternary.

--- PSR2R/Sniffs/ControlStructures/TernarySpacingSniff.php
+++ PSR2R/Sniffs/ControlStructures/TernarySpacingSniff.php
@@ -6,6 +6,10 @@ use PHP_CodeSniffer_Tokens;
 
 /**
  * Asserts no whitespace between ternary operator (? and :) and surroundings.
+ *
+ * @see https://github.com/dereuromark/codesniffer-standards/blob/master/MyCakePHP/Sniffs/WhiteSpace/TernarySpacingSniff.php
+ * @author Mark Scherer
+ * @license MIT
  */
 class TernarySpacingSniff extends \PSR2R\Tools\AbstractSniff {
 
@@ -55,6 +59,7 @@ class TernarySpacingSniff extends \PSR2R\Tools\AbstractSniff {
 	protected function assertSpaceBefore(PHP_CodeSniffer_File $phpcsFile, $stackPtr) {
 		$previous = $phpcsFile->findPrevious(T_WHITESPACE, ($stackPtr - 1), null, true);
 		if ($stackPtr - $previous > 1) {
+			$this->assertSingleSpaceBeforeIfNotMultiline($phpcsFile, $stackPtr, $previous);
 			return;
 		}
 
@@ -75,6 +80,7 @@ class TernarySpacingSniff extends \PSR2R\Tools\AbstractSniff {
 	protected function assertSpaceAfter(PHP_CodeSniffer_File $phpcsFile, $stackPtr) {
 		$nextIndex = $phpcsFile->findNext(T_WHITESPACE, ($stackPtr + 1), null, true);
 		if ($nextIndex - $stackPtr > 1) {
+			$this->assertSingleSpaceAfterIfNotMultiline($phpcsFile, $stackPtr, $nextIndex);
 			return;
 		}
 
@@ -84,6 +90,50 @@ class TernarySpacingSniff extends \PSR2R\Tools\AbstractSniff {
 		$fix = $phpcsFile->addFixableError($error, $stackPtr, 'SpaceAfterInlineThen');
 		if ($fix) {
 			$phpcsFile->fixer->addContent($stackPtr, ' ');
+		}
+	}
+
+	/**
+	 * @param \PHP_CodeSniffer_File $phpcsFile
+	 * @param int $stackPtr
+	 * @param int $previous
+     */
+	protected function assertSingleSpaceBeforeIfNotMultiline(PHP_CodeSniffer_File $phpcsFile, $stackPtr, $previous) {
+		$tokens = $phpcsFile->getTokens();
+
+		if ($tokens[$stackPtr]['line'] !== $tokens[$previous]['line']) {
+			return;
+		}
+		if ($tokens[$stackPtr - 1]['content'] === ' ') {
+			return;
+		}
+
+		$error = 'There must be a single space instead of `' . $tokens[$stackPtr - 1]['content'] . '`';
+		$fix = $phpcsFile->addFixableError($error, $stackPtr - 1, 'InvalidSpaceBefore');
+		if ($fix) {
+			$phpcsFile->fixer->replaceToken($stackPtr - 1, ' ');
+		}
+	}
+
+	/**
+	 * @param \PHP_CodeSniffer_File $phpcsFile
+	 * @param int $stackPtr
+	 * @param int $previous
+	 */
+	protected function assertSingleSpaceAfterIfNotMultiline(PHP_CodeSniffer_File $phpcsFile, $stackPtr, $next) {
+		$tokens = $phpcsFile->getTokens();
+
+		if ($tokens[$stackPtr]['line'] !== $tokens[$next]['line']) {
+			return;
+		}
+		if ($tokens[$stackPtr + 1]['content'] === ' ') {
+			return;
+		}
+
+		$error = 'There must be a single space instead of `' . $tokens[$stackPtr + 1]['content'] . '`';
+		$fix = $phpcsFile->addFixableError($error, $stackPtr + 1, 'InvalidSpaceAfter');
+		if ($fix) {
+			$phpcsFile->fixer->replaceToken($stackPtr + 1, ' ');
 		}
 	}
 
