ba8b188d681686420b2eaac2de20140d307f49d6
Mark Scherer <euromark@web.de>
2016-03-18 17:12:16 +0100

    CS

--- PSR2R/Sniffs/Files/ClosingTagSniff.php
+++ PSR2R/Sniffs/Files/ClosingTagSniff.php
@@ -23,7 +23,6 @@ use PHP_CodeSniffer_Sniff;
  *
  * Checks that the file does not end with a closing tag.
  *
- *
  * @author Greg Sherwood <gsherwood@squiz.net>
  * @copyright 2006-2014 Squiz Pty Ltd (ABN 77 084 670 600)
  * @license https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.txt BSD Licence
