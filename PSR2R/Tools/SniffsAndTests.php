baade2fbadd04f3e5290389ff3de3b974ea02aea
mscherer <dereuromark@web.de>
2023-10-15 02:55:58 +0200

    Fix CS

--- PSR2R/Tools/SniffsAndTests.php
+++ PSR2R/Tools/SniffsAndTests.php
@@ -12,48 +12,48 @@ use RecursiveIteratorIterator;
 use RecursiveRegexIterator;
 use RegexIterator;
 
-class SniffsAndTests
-{
-    /**
-     * @var array<string>
-     */
-    protected static $orgs = [
-        'PSR2R',
-    ];
+class SniffsAndTests {
 
-    /**
-     * @param string $path Path
-     *
-     * @return array<string, array<string, mixed>>
-     */
-    public function untested(string $path): array
-    {
-        $path = rtrim($path, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
+	/**
+	 * @var array<string>
+	 */
+	protected static $orgs = [
+		'PSR2R',
+	];
 
-        $sniffs = [];
+	/**
+	 * @param string $path Path
+	 *
+	 * @return array<string, array<string, mixed>>
+	 */
+	public function untested(string $path): array {
+		$path = rtrim($path, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
 
-        foreach (static::$orgs as $org) {
-            $directoryIterator = new RecursiveDirectoryIterator($path . $org);
-            $recursiveIterator = new RecursiveIteratorIterator($directoryIterator);
-            $regexIterator = new RegexIterator($recursiveIterator, '#^.+/(\w+)/Sniffs/(\w+)/(\w+)Sniff\.php$#', RecursiveRegexIterator::GET_MATCH);
+		$sniffs = [];
 
-            foreach ($regexIterator as $match) {
-                $org = $match[1];
-                $type = $match[2];
-                $name = $match[3];
-                $testFile = $path . 'tests' . DIRECTORY_SEPARATOR . $org . 'Sniffs' . DIRECTORY_SEPARATOR . $type . DIRECTORY_SEPARATOR . $name . 'SniffTest.php';
-                $hasTest = file_exists($testFile);
-                if ($hasTest) {
-                    continue;
-                }
+		foreach (static::$orgs as $org) {
+			$directoryIterator = new RecursiveDirectoryIterator($path . $org);
+			$recursiveIterator = new RecursiveIteratorIterator($directoryIterator);
+			$regexIterator = new RegexIterator($recursiveIterator, '#^.+/(\w+)/Sniffs/(\w+)/(\w+)Sniff\.php$#', RecursiveRegexIterator::GET_MATCH);
 
-                $key = $org . '.' . $type . '.' . $name;
-                $sniffs[$key] = [
-                    'hasTest' => $hasTest,
-                ];
-            }
-        }
+			foreach ($regexIterator as $match) {
+				$org = $match[1];
+				$type = $match[2];
+				$name = $match[3];
+				$testFile = $path . 'tests' . DIRECTORY_SEPARATOR . $org . 'Sniffs' . DIRECTORY_SEPARATOR . $type . DIRECTORY_SEPARATOR . $name . 'SniffTest.php';
+				$hasTest = file_exists($testFile);
+				if ($hasTest) {
+					continue;
+				}
 
-        return $sniffs;
-    }
+				$key = $org . '.' . $type . '.' . $name;
+				$sniffs[$key] = [
+					'hasTest' => $hasTest,
+				];
+			}
+		}
+
+		return $sniffs;
+	}
+
 }
