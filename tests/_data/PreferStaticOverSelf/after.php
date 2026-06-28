f02d63a8b560c66bd5336d7651ec118ae86607b4
Mark Scherer <dereuromark@users.noreply.github.com>
2026-06-28 13:12:21 +0200

    Skip final classes in static-over-self sniff

---
+++ tests/_data/PreferStaticOverSelf/after.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types = 1);
+
+namespace PSR2R;
+
+class FixMe {
+
+	public const VALUE = 'value';
+
+	public function value(): string {
+		return static::VALUE;
+	}
+
+}
+
+final class KeepMe {
+
+	public const VALUE = 'value';
+
+	public function value(): string {
+		return self::VALUE;
+	}
+
+}
